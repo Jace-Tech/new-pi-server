020da5c9146e8aabb3b2e111e85466d471064310
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2024-05-31 17:30:36 +0100

    done

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'soundpy-api',
-      script: './server.js',
+      script: './build/bin/server.js',
       instances: 'max',
       exec_mode: 'cluster',
       autorestart: true,
